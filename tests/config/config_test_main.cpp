4be6dd1e0bb47e73ff955689f421c0dc2bebe490
Arseniy SHISHAEV <ashishae@student.42.fr>
2021-03-02 12:56:42 +0100

    Rename unit_tests to config_test_main.cpp

--- tests/config/config_test_main.cpp
+++ tests/config/config_test_main.cpp
@@ -6,7 +6,7 @@
 /*   By: ashishae <ashishae@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/12/17 17:45:02 by ashishae          #+#    #+#             */
-/*   Updated: 2021/02/19 11:41:34 by ashishae         ###   ########.fr       */
+/*   Updated: 2021/03/02 12:54:59 by ashishae         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,55 +14,9 @@
 // Test-related stuff below
 
 // Needed to automatically fail tests;
-#include <assert.h>
-#include <string>
 
 #include <iostream>
 
-// bool exception_thrown = false;
-
-// void check(int expression);
-
-// #define TEST_EXCEPTION(expression, exceptionType, exceptionString) { \
-// 	exception_thrown = false; \
-// 	try \
-// 	{ \
-// 		expression; \
-// 	} \
-// 	catch (const exceptionType &e) \
-// 	{ \
-// 		exception_thrown = true; \
-// 		check((!strcmp(e.what(), exceptionString))); \
-// 		std::cout << e.what() << std::endl;\
-// 	} \
-// 	if (!exception_thrown)\
-// 		std::cout << "Exception NOT thrown" << std::endl; \
-// 	check(exception_thrown == true); \
-// }
-
-// int test_no = 1;
-
-// void out(std::string s)
-// {
-// 	std::cout << std::endl;
-// 	std::cout << "\033[0;34m" << "Test " << test_no << " | " << s << "\033[0m" << std::endl;
-// 	test_no += 1;
-// }
-
-// void check(int expression)
-// {
-// 	// If expression doesn't evaluate to 1, the program will abort
-// 	// assert(expression == 1);
-// 	if (expression == 1)
-// 	{
-// 		std::cout << "\033[92m✓ PASS\033[0m" << std::endl;
-// 	}
-// 	else 
-// 	{
-// 		std::cout << "\033[91m✓ FAIL\033[0m" << std::endl;
-// 	}
-	
-// }
 
 # include "femtotest.hpp"
 
--- tests/test_config.sh
+++ tests/test_config.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-clang++ ../src/config/*.cpp ./config/unit_tests.cpp ../src/get_next_line/*.cpp ../src/Utility.cpp -I ../inc/ -o config_test -Wall -Wextra -Werror && ./config_test
+clang++ ../src/config/*.cpp ./config/config_test_main.cpp ../src/get_next_line/*.cpp ../src/Utility.cpp -I ../inc/ -o config_test -Wall -Wextra -Werror && ./config_test
 rm config_test
