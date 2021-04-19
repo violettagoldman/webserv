52fcaa7430e873096fdc7183333c0efc37becc42
Arseny Shishaev <ashishae@student.42.fr>
2021-04-19 17:26:07 +0100

    Fill boilerplate for Location

--- inc/PasswordFile.class.hpp
+++ inc/PasswordFile.class.hpp
@@ -6,7 +6,7 @@
 /*   By: ashishae <ashishae@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/27 13:23:55 by ashishae          #+#    #+#             */
-/*   Updated: 2021/03/27 16:29:25 by ashishae         ###   ########.fr       */
+/*   Updated: 2021/04/19 17:24:27 by ashishae         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,12 +30,13 @@ class PasswordFile
 {
 public:
 	PasswordFile(std::string filename);
-	// PasswordFile(const PasswordFile &copy);
-	// PasswordFile &operator= (const PasswordFile &operand);
-	// ~PasswordFile();
+	PasswordFile(const PasswordFile &copy);
+	PasswordFile &operator= (const PasswordFile &operand);
+	~PasswordFile();
 	std::vector<Credential> getCredentials(void) const;
 
 private:
+	PasswordFile(void);
 	std::string _filename;
 	ConfigFile _confFile;
 	std::vector<Credential> _credentials;
